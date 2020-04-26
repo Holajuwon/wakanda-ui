e7f89b37e4203c9d47e4ad7d2d45c05ee9ead0f0
Holajuwon <henrijay00@gmail.com>
2020-04-26 15:12:25 +0100

    added react buildpack

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import AllPosts from "./Pages/AllPosts";
+import Home from "./Pages/Home";
 
 test("renders Chrakra UI in App", () => {
-  const { getByText } = render(<AllPosts />);
-  const element = getByText(/Latest Post/i);
+  const { getByText } = render(<Home />);
+  const element = getByText(/Wakanda/i);
   expect(element).toBeInTheDocument();
 });
